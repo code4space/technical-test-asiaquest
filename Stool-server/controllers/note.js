9114e59e68a906551d56af1cccff62fa948834c4
William <mauritius.william.wijaya1@gmail.com>
2023-06-24 19:58:39 +0700

    CRUD API quick-note

--- Stool-server/controllers/note.js
+++ Stool-server/controllers/note.js
@@ -1,7 +1,79 @@
-class Note {
-  static async signIn(req, res, next) {
+const {
+  Teacher,
+  Task,
+  Student,
+  StudentTasks,
+  Notification,
+  StudentNotification,
+  Note,
+} = require("../models");
+
+class NoteController {
+  static async getQuickNote(req, res, next) {
     try {
-      return res.status(200).json({ message: "mantap" });
+      const { role, id } = req.user;
+      let note;
+      if (role === "student") {
+        note = await Note.findAll({
+          where: {
+            StudentId: id,
+          },
+          attributes: ["id", "title", "description"],
+        });
+      } else {
+        note = await Note.findAll({
+          where: {
+            TeacherId: id,
+          },
+          attributes: ["id", "title", "description"],
+        });
+      }
+
+      return res.status(200).json({ note });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  static async updateQuickNote(req, res, next) {
+    try {
+      const { title, description } = req.body;
+      await Note.update(
+        { title, description },
+        {
+          where: { id: +req.params.id },
+        }
+      );
+      return res.status(202).json({ message: "Update Note success" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  static async addQuickNote(req, res, next) {
+    const { role, id } = req.user;
+    const { title, description } = req.body;
+
+    if (role === "student") {
+      await Note.create({ title, description, StudentId: id });
+    } else {
+      await Note.create({ title, description, TeacherId: id });
+    }
+    return res.status(201).json({ message: "new Note has been created" });
+  }
+  catch(error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+
+  static async deleteQuickNote(req, res, next) {
+    try {
+      await Note.destroy({
+        where: { id: +req.params.id },
+      });
+      return res.status(200).json({ message: "note deleted successfully" });
     } catch (error) {
       console.log(error);
       res.status(500).json({ message: "Internal Server Error" });
@@ -9,4 +81,4 @@ class Note {
   }
 }
 
-module.exports = Note;
+module.exports = NoteController;
--- Stool-server/router/router.js
+++ Stool-server/router/router.js
@@ -3,7 +3,7 @@ const validation = require('../helper/authentication')
 const AI = require("../controllers/ai");
 const TeacherController = require("../controllers/teacher");
 const StudentController = require("../controllers/student");
-const Note = require("../controllers/note");
+const NoteController = require("../controllers/note");
 
 // Login/Register API
 route.post("/teacher/login", TeacherController.login);
@@ -24,6 +24,12 @@ route.get("/student/task", StudentController.getMyTask);
 // notification
 route.get("/notification", StudentController.getMyNotification);
 route.patch("/notification", StudentController.readNotification);
+
+// Note
+route.get("/quick-note", NoteController.getQuickNote);
+route.patch("/quick-note/:id", NoteController.updateQuickNote);
+route.post("/quick-note", NoteController.addQuickNote);
+route.delete("/quick-note/:id", NoteController.deleteQuickNote);
 
 // AI API
 route.post("/ask-ai", AI.askAI);
